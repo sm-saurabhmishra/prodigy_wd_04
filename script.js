a654c1f61a3f6db5ecfdbd947cdc398e5945cbb8
saurabh mishra <164666359+sm-saurabhmishra@users.noreply.github.com>
2024-05-27 22:34:03 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+document.getElementById('contact-form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert('Thank you for your message!');
+    // You can add further form submission logic here
+});
